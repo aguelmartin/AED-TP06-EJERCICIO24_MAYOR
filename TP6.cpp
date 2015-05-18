18986ccb68b6cf7104b8f595059fa656cf51b4ab
aguelmartin <martin.aguel@yahoo.com>
2015-05-17 22:27:55 -0300

    TP06

---
+++ TP6.cpp
@@ -0,0 +1,30 @@
+/* TP6 EJ24
+ * MARTIN DANIEL AGUEL
+ * 20150518
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a, b,c;
+
+cout<<"Ingrese el 1er numero Natural:"<<endl;
+cin>>a;
+cout<<"Ingrese el 2do numero Natural:"<<endl;
+cin>>b;
+cout<<"Ingrese el 3er numero Natural:"<<endl;
+cin>>c;
+
+if(a>b,a>c){
+    cout<<"El Mayor numero es: "<< a <<endl;
+}
+else if(b>a,b>c){
+    cout<<"El Mayor numero es: "<< b <<endl;
+    }
+else if(c>a,c>b){
+    cout<<"El Mayor numero es: "<< c <<endl;
+    }
+
+}
